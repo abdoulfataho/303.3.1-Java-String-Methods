a5fd160d9284e26e7bdc104e1aaa7b6c6a08382d
abdoulfataho <yamyaogofataho@gmail.com>
2024-07-02 08:52:01 -0400

    lenght method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str1 = "java";
+        String str2 = "";
+        System.out.println(str1.length());
+        System.out.println(str2.length());
+        System.out.println("java".length());
+        System.out.println("java\n".length());
+        System.out.println("learn java".length());
+
     }
 }
