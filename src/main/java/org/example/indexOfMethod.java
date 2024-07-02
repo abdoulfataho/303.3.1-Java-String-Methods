125972c2680ce5ad7c4ec5066a2776cf2b77bbc8
abdoulfataho <yamyaogofataho@gmail.com>
2024-07-02 10:10:45 -0400

    index od method

---
+++ src/main/java/org/example/indexOfMethod.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class indexOfMethod {
+    public static void main(String[] args) {
+        String str1 = "Java is fun";
+        int result;
+        result = str1.indexOf('s');
+
+        System.out.println(result);
+
+    }
+}
