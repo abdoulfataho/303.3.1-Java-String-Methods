eca86cc0f3d891a910004dff34e66e98ccd818d0
abdoulfataho <yamyaogofataho@gmail.com>
2024-07-02 08:58:19 -0400

    is emptu method

---
+++ src/main/java/org/example/IsEmptyMethod.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class IsEmptyMethod {
+    public static void main(String[] args) {
+        String s1="";
+        String s2 ="hello";
+        System.out.println(s1.isEmpty());
+        System.out.println(s2.isEmpty());
+
+    }
+}
